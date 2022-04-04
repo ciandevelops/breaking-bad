329b35406dc46c6c6aa199a5aaf8f60ef0bd9f60
ciandevelops <ciandevelops@gmail.com>
2022-04-04 11:06:31 -0400

    header created and styled

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Header from "./components/ui/Header";
 
 function App() {
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <Header />
     </div>
   );
 }
