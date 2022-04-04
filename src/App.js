f30a1e0728994f9585cbfe2126c36b976ecdf11d
ciandevelops <ciandevelops@gmail.com>
2022-04-04 13:46:29 -0400

    search added and styled

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/ui/Header";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import CharacterGrid from "./components/characters/CharacterGrid";
+import Search from "./components/ui/Search";
 
 function App() {
   const [items, setItems] = useState([]);
@@ -24,6 +25,7 @@ function App() {
   return (
     <div className="container">
       <Header />
+      <Search />
       <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
